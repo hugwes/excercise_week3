2454c243b33a4ff3542cdcf3f0ca08829b701240
Wesley Hug <hugwes@students.zhaw.ch>
2021-05-03 22:29:04 +0200

    starting excercise 3

---
+++ solutions_week3.R
@@ -0,0 +1,31 @@
+
+### EXCERCISE 3 ###
+
+##########################################################
+# Preparations
+# https://github.com/hugwes/excercise_week3 (github-link)
+
+##########################################################
+# Load in Libraries
+library(readr)        # to import tabular data (e.g. csv)
+library(dplyr)        # to manipulate (tabular) data
+library(ggplot2)      # to visualize data
+library(sf)           # to handle spatial vector data
+library(terra)        # to handle raster data
+library(lubridate)    # to handle dates and times
+library(zoo)          # moving window functions
+
+# Import data
+
+# Convert into sf-object
+
+##########################################################
+# Task 1: 
+
+
+
+
+
+
+
+
